21246d9ac9cbaaa628044526d66384ce91f1e54a
Shai Geffen <74376404+shai987@users.noreply.github.com>
2023-04-05 12:02:50 +0300

    Delete src/tests directory

--- src/tests/counter.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Breads from "../components/Grains&Bakery/Breads";
-
-test('testing the result element', () => {
-        render(<Breads />);
-        const counterDivText = screen.getByText(/מספר מנות/);
-        console.log(counterDivText.textContent);
-        expect(counterDivText.textContent).toContain('')
-});
