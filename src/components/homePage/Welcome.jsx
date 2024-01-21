d6ddcd641ed22289d59dbdee77a2326606252e10
Shai Geffen <74376404+shai987@users.noreply.github.com>
2024-01-21 11:50:41 +0200

    Add home page image

--- src/components/homePage/Welcome.jsx
+++ src/components/homePage/Welcome.jsx
@@ -1,10 +1,19 @@
-const Welcome = () => {
+import Image from '../Image';
+import '../../assets/css/Welcome.css';
 
+const Welcome = () => {
+        const image = {
+                src: require('../../assets/images/HomepageImage.jpg'),
+                alt: "תמונה דף בית",
+                title: "תמונה דף בית",
+                style: {}
+        }
         return (
                 <div className="welcome" >
                         <h1>ברוך הבא לאתר קבוצות תחליף</h1>
                         <p>האתר מסייע בחישוב כמות מנות לפי קטגוריות</p>
-                </div>
+                        <Image img={image} />
+                </div >
         );
 }
 
